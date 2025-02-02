6d60910fb33e2c1782c24628bf6966cb667b98f4
FylypekUNO <filip.dudzik.uno@gmail.com>
2025-02-02 21:47:31 +0100

    No XML

--- app/src/main/java/pl/fylypek/librus_unofficial/MainActivity.kt
+++ app/src/main/java/pl/fylypek/librus_unofficial/MainActivity.kt
@@ -4,19 +4,16 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val defaultView = TextView(this)
+        defaultView.text = "Loading..."
+
+        setContentView(defaultView)
 
         val url = "https://jsonplaceholder.typicode.com/todos/1"
         fetch(url)
@@ -29,7 +26,8 @@ class MainActivity : AppCompatActivity() {
                 return@then outputView
             }
             .then { outputView ->
-                outputView.textSize = 24f
+                outputView.textSize = 18f
+                outputView.setPadding(1*3, 32*3, 0, 0)
             }
             .catch { error ->
                 val outputView = TextView(this)
